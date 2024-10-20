301d681943c8863f40c8bde97e9d0ff63e2a93c5
太卜『符玄』 <fuxuan001@foxmail.com>
2024-10-20 20:01:49 +0800

    Add files via upload

---
+++ example_usage.py
@@ -0,0 +1,18 @@
+from FuXLogger import LogFormatter , Level , LogManager , StreamHandler
+import time
+from datetime import timedelta
+
+fmt = LogFormatter("{time} | {levelName:<8} | {name}.{module}.{function} | {file}:{line:03d} | {message}")
+logger = LogManager.getLogger("test", Level.ON, fmt, enqueue=True)
+console_handler = StreamHandler("console", Level.ON, fmt, enableXMLRender=True, colorize=True)
+logger.addHandler(console_handler)
+
+start = time.time()
+for i in range(10000):
+    logger.trace(f"This is a trace message {i}")
+end = time.time()
+
+elapsed = end - start
+to_delta = timedelta(seconds=elapsed)
+
+logger.info(f"Execution time: {to_delta}")
