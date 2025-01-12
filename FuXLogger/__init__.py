e6fd59c888fbacf6acc5ecc1d1799de3c794c76e
太卜『符玄』 <fuxuan001@foxmail.com>
2025-01-12 17:04:04 +0800

    Delete FuXLogger directory because we want upgrade it

--- FuXLogger/__init__.py
+++
@@ -1,9 +0,0 @@
-
-from .core import LogManager , LogFormatter, StreamHandler , Handler , Level , LogLevel
-from .core.handlers import FileHandler , SocketHandler
-
-__all__ = [
-    'LogManager', 
-    'LogFormatter', 'StreamHandler', "FileHandler", "SocketHandler",
-    'Handler', 'Level', 'LogLevel'
-]
--- FuXLogger/core/__init__.py
+++
@@ -1,8 +0,0 @@
-
-from .handlers import StreamHandler , Handler
-from .logger import Logger
-from .loglevel import Level , LogLevel
-from .formatter import LogFormatter
-from .LogManager import LogManager
-
-__all__ = ['StreamHandler', 'Handler', 'Logger', 'LogFormatter', 'LogManager', 'Level']
